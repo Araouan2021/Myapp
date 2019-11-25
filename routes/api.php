80502e59efbdb2332273fecea9ebd2ee2d924dd6
Bola Bally <bolabally@gmail.com>
2019-11-25 19:07:14 +0000

    add api.php

--- routes/api.php
+++ routes/api.php
@@ -39,3 +39,17 @@ Route::delete('articles/{id}', function($id) {
 
     return 204;
 });
+
+/*
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
+*/
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
