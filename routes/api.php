24a1a5237aafdbd7e63924103b1a743bfd92f04e
Bola Bally <bolabally@gmail.com>
2019-12-04 10:55:03 +0000

    add Handler.php

--- routes/api.php
+++ routes/api.php
@@ -13,41 +13,14 @@ Use App\Article;
 |
 */
 
-/*
-Route::get('articles', function() {
-    // If the Content-Type and Accept headers are set to 'application/json', 
-    // this will return a JSON structure. This will be cleaned up later.
-    return Article::all();
-});
- 
-Route::get('articles/{id}', function($id) {
-    return Article::find($id);
+Route::middleware('auth:api')
+    ->get('/user', function (Request $request) {
+        return $request->user();
 });
 
-Route::post('articles', function(Request $request) {
-    return Article::create($request->all);
-});
-
-Route::put('articles/{id}', function(Request $request, $id) {
-    $article = Article::findOrFail($id);
-    $article->update($request->all());
-
-    return $article;
-});
-
-Route::delete('articles/{id}', function($id) {
-    Article::find($id)->delete();
-
-    return 204;
-});
-
-/*
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{id}', 'ArticleController@update');
-Route::delete('articles/{id}', 'ArticleController@delete');
-*/
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
+Route::post('register', 'Auth\RegisterController@register');
 
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('articles', 'ArticleController@index');
@@ -56,17 +29,4 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::put('articles/{article}', 'ArticleController@update');
     Route::delete('articles/{article}', 'ArticleController@delete');
 });
-
-Route::post('register', 'Auth\RegisterController@register');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout');
-
-/*
-Route::middleware('auth:api')
-    ->get('/user', function (Request $request) {
-        return $request->user();
-    });
-Auth::guard('api')->user(); // instance of the logged user
-Auth::guard('api')->check(); // if a user is authenticated
-Auth::guard('api')->id(); // the id of the authenticated user
 
