fc6c9df59451907289c8a9f450307786f9fe2679
Bola Bally <bolabally@gmail.com>
2019-12-09 19:44:50 +0000

    add RegisterTest.php

--- tests/Feature/Feature/LoginTest.php
+++ tests/Feature/Feature/LoginTest.php
@@ -43,15 +43,5 @@ class LoginTest extends TestCase
 
     }
 }
-    /**
-     * A basic feature test example.
-     *
-     //@return void
-     */
-    //public function testExample()
-    //{
-        //$response = $this->get('/');
-
-        //$response->assertStatus(200);
-    //}
+   
 
